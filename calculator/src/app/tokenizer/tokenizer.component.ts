bb61529dbab28bb839839e88496745d282777221
ty <tailstylerturnquist@gmail.com>
2025-04-02 11:49:29 -0500

    .

--- calculator/src/app/tokenizer/tokenizer.component.ts
+++ calculator/src/app/tokenizer/tokenizer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { tokenize } from './tokenizer.service';
-import { lex, Num_Node } from './lexing.service';
+import { lex, lex0, Num_Node } from './lexing.service';
 import { UnaryOperator } from '@angular/compiler';
 
 @Component({
@@ -26,11 +26,11 @@ export class TokenizerComponent implements OnInit {
     // this.value = tokenize(math_expression);
 
     //my attempt
-    this.value = (lex(tokenize(math_expression)) as Num_Node).value;
+    this.value = (lex0(tokenize(math_expression)) as Num_Node).value;
     console.log(math_expression);
     console.log(tokenize(math_expression));
-    console.log(lex(tokenize(math_expression)) as Num_Node);
-    console.log((lex(tokenize(math_expression)) as Num_Node).value);
+    console.log(lex0(tokenize(math_expression)) as Num_Node);
+    console.log((lex0(tokenize(math_expression)) as Num_Node).value);
     console.log(this.value);
   }
 
